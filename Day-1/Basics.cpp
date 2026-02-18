b0b2ccfb0ff2d454fd1ffb80cc2c4e64a0e611bb
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2026-02-18 23:28:45 +0530

    Day 1 learning

---
+++ Day-1/Basics.cpp
@@ -0,0 +1,24 @@
+/*      Learnt about the flowchart and its components
+                 
+            we have 5 components
+                 1. Terminator (start / end)
+                 2. Input / Output
+                 3. Process
+                 4. Decision Making
+                 5. Arrows (connector)
+                 
+*/
+
+/*               Learnt about Pseudocode
+    
+Its the way to represent the code in natural language for understanding
+
+example : write a pseudo code to adding of 2 numbers
+
+pseudocode: 
+            step 1: start
+            step 2: read input a and b
+            step 3: add the values of a and b and store it in new variable
+            step 4: print the variable
+            step 5: end
+*/
