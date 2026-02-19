2c639c9e624c5c7bc0eadf071b29ce336e0ba01e
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2026-02-20 00:19:21 +0530

    First Program

---
+++ Day-2/Operators.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 2;
+    float b = 3;
+
+    // Arithmetic Operators
+
+   
+    int d = a+b; // addition
+
+    int e = a*b; // multiplication
+
+    int f = b-a; //subtraction
+
+    double c = b/a; //division
+
+    bool a1 = true; // prints binary of true
+    bool a2 = false; // prints binary of false
+
+    cout<<a<<endl; 
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+    cout<<a2<<endl;
+    cout<<a1;
+
+    // Arithmetic Operators
+
+    // Relational Operators
+
+    cout<<(a==b)<<endl; // if a and b value are equal it prints true(1) in binary else false(0) in binary
+
+    cout<<(a!=b)<<endl; // if a and b value are not equal it prints true(1) in binary else false(0) in binary
+
+    cout<<(a>=b)<<endl; // if a value is greater than b it prints true(1) in binary else false(0) in binary
+
+    cout<<(a<=b); // if b value is greater than a it prints true(1) in binary else false(0) in binary
+
+}
