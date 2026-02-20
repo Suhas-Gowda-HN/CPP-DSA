b12754b3b38004748a00484522d5aa62f24db25b
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2026-02-20 12:27:13 +0530

    Alternative code for Homework file

--- Day-3/If-Else/Homework.cpp
+++ Day-3/If-Else/Homework.cpp
@@ -2,20 +2,37 @@
 using namespace std;
 
 int main() {
-    char ch;
-    cout << "Enter a Input: ";
-    cin >> ch;
+    // char ch;
+    // cout << "Enter a Input: ";
+    // cin >> ch;
 
-    if (ch >= 'a' && ch <= 'z') {
-        cout << "Lowercase";
+    // if (ch >= 'a' && ch <= 'z') {
+    //     cout << "Lowercase";
+    // }
+    // else if (ch >= 'A' && ch <= 'Z') {
+    //     cout << "Uppercase";
+    // }
+    // else if (ch >= '0' && ch <= '9') {
+    //     cout << "Numeric";
+    // }
+    // else {
+    //     cout << "Special";
+    // }
+
+    int a ;
+    cout<<"Enter any character of your choice: "<<endl;
+    a = cin.get();
+
+    if(a>=48 && a<=57){
+        cout<<"Numeric number"<<endl;
     }
-    else if (ch >= 'A' && ch <= 'Z') {
-        cout << "Uppercase";
+    else if(a>=65 && a<=90){
+        cout<<"Uppercase Character"<<endl;
     }
-    else if (ch >= '0' && ch <= '9') {
-        cout << "Numeric";
+    else if(a>=97 && a<=122){
+        cout<<"Lowercase Character"<<endl;
     }
-    else {
-        cout << "Special";
+    else{
+        cout<<"Special Character";
     }
 }
