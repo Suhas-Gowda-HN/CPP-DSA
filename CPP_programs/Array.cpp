1faa8be641920977a6ee27f78a8e6c43353ce847
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2026-03-03 22:13:21 +0530

    array initialization

---
+++ CPP_programs/Array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+    //int a[];   cannot be done because the stack needs size 
+    
+    // therefore
+
+    int a[]={0,1,2,3,4,5};
+
+    //or
+
+    int a[4];
+    a[0]=1;
+    a[1]=2;
+    a[2]=3;
+    a[3]=4;
+    a[4]=5;
+
+
+
+ }
